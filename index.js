fe2f5b0d39b6a991fd095a56ce542c30308e0bc6
whatislavj <vlad.jechel@gmail.com>
2023-09-26 21:20:12 +0300

    done

---
+++ index.js
@@ -0,0 +1,52 @@
+//task2
+const ziuaSaptamanii = 'Vineri';
+
+switch (ziuaSaptamanii) {
+    case 'Luni':
+        console.log('Azi e luni.');
+        break;
+    case 'Marti':
+        console.log('Azi e marti.');
+        break;
+    case 'Miercuri':
+        console.log('Azi e miercuri.');
+        break;
+    case 'Joi':
+        console.log('Azi e joi.');
+        break;
+    case 'Vineri':
+        console.log('Azi e vineri.');
+        break;
+    case 'Sambata':
+        console.log('Azi e sambata.');
+        break;
+    case 'Duminica':
+        console.log('Azi e duminica.');
+        break;
+    default:
+        console.log('Nu e zi a saptamanii.')
+}
+console.log(' ');
+
+//task3
+let str = 'abcdefghijklMNOPQrstuvwxyz';
+if (str.length > 10) {
+    console.log('lungimea mai mare ca 10');
+} else if (str.length < 10) {
+    console.log('lungimea mai mica ca 10');
+} else {
+    console.log ('lungimea egala cu 10');
+};
+console.log(' ');
+
+//task4
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(' ');
+
+//task5
+let str1 = "Audi, BMW, Toyota, Nissan, Mazda";
+let str2 = str1.slice(11, 17)
+console.log(str1, '    ', str2)
+let str3 = str1.split(' ')[2];
+console.log(str1, '    ', str3)
